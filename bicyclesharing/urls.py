2d07739cd97155edc77df3546d0b5c9f2098fcd7
Mizanur Rahman <mizan.441980@gmail.com>
2018-11-16 15:08:20 +0600

    pots added

--- bicyclesharing/urls.py
+++ bicyclesharing/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
 ]
